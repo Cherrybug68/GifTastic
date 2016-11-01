7dc9d2c270dbaecda6666c53661309c262733839
Shari Smith <Cherrybug68@gmail.com>
2016-11-01 17:47:19 -0400

    TuesdayPMUpdate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,50 @@
-// Initial array of TV Shows
-var tvShows = ['Revolution', 'How to Get Away With Murder', 'M*A*S*H'];
+"use strict";
 
-// Display the TV Show pics
+// Initial array of dogs
+var dogs = [
+	'Yorkshire Terrier',
+	'Weimaraner',
+	'Dachshund'];
+
+// Get Dog image data
 function getData()
-	{ $('#tvShowView').empty();
+	{
+	$('#dogView').empty();
 
-	var tvShow = $(this).attr('data-name');
-	console.log(tvShow);
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + tvShow + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var dog = $(this).attr('data-name');
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dog + "&breed&api_key=dc6zaTOxFJmzC&limit=10";
 
-	// AJAX call for the specific tv show
+	// AJAX call to get data
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response)
-		{// console.log(response);
-
+		{
 		var results = response.data;
-		console.log(results.length);
 
 		for (var i = 0; i < results.length; i++)
-			{// Generic divs to hold tv show
-
+			{
 			var div = $("<div>").attr("class", "img");
 
 			div.append("<p>Rating: " + results[i].rating + "</p>");
-			// console.log(tvShow);
 
-			div.append('<p><img src = "' + results[i].images.fixed_height.url + '"></p>');
+			div.append('<p><img src = "' + results[i].images.fixed_height_still.url + '"></p>');
 
-			$("#tvShowView").append(div);
+			$("#dogView").append(div);
+			};
 
-			console.log(results[i].images.fixed_height.url);
-		};
-	});
+			$('img').on('click', toggleStill);
+
+			function toggleStill()
+				{
+				if (state == 'still'){
+					$(this).attr('src', $(this).data('animate'));
+					$(this).attr('data-state', 'animate');
+				} else {
+					$(this).attr('src', $(this).data('still'));
+					$(this).attr('data-state', 'still');
+				}
+			}
+		});
 }
+
 
 // function for displaying buttons
 function renderButtons()
@@ -39,25 +52,23 @@ function renderButtons()
 	$('#buttonsView').empty();
 
 	// Loop throught array to create the buttons
-	for (var i = 0; i < tvShows.length; i++){
+	for (var i = 0; i < dogs.length; i++){
 
-	createButton(tvShows[i]);
+	createButton(dogs[i]);
 	}
 	// event listener for button with class tv
-	$('.tv').on('click', getData);
+	$('.dog').on('click', getData);
 	}
 
 
 // renders new/additional button when submit button is clicked
-$('#addTV').on('click', function()
-	{ var tvShow = $('#show-input').val().trim();
+$('#addDog').on('click', function()
+	{ var dog = $('#dog-input').val().trim();
 
-	// tvShows.push(tvShow);
-
-	createButton(tvShow);
+	createButton(dog);
 
 	// event listener for button with class tv
-	$('.tv').on('click', getData);
+	$('.dog').on('click', getData);
 	return false;
 	});
 
@@ -65,16 +76,16 @@ $('#addTV').on('click', function()
 function createButton(value)
 	{ var a = $('<button>');
 
-		a.attr("class",'tv');
+		a.attr("class",'dog');
 		a.attr('data-name', value);
 		console.log(a.attr("data-name"));
 		a.text(value);
 		$('#buttonsView').append(a);
-
 	}
 
-renderButtons();
 
+
+renderButtons();
 
 
 
